f201cd740b35b5880015a81f8c48e9c6a2486d8b
artem-inkaz <artem.inkaz@gmail.com>
2024-08-18 09:17:44 +0500

    [home_work_3] add search movie logic

--- app/src/main/java/ru/androidschool/intensiv/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/search/SearchFragment.kt
@@ -1,14 +1,31 @@
 package ru.androidschool.intensiv.ui.search
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import io.reactivex.Observable
+import io.reactivex.ObservableOnSubscribe
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import ru.androidschool.intensiv.R
+import ru.androidschool.intensiv.data.MovieLocal
+import ru.androidschool.intensiv.data.mappers.MovieSearchMapper
 import ru.androidschool.intensiv.databinding.FeedHeaderBinding
 import ru.androidschool.intensiv.databinding.FragmentSearchBinding
+import ru.androidschool.intensiv.network.MovieApiClient
+import ru.androidschool.intensiv.ui.afterTextChanged
+import ru.androidschool.intensiv.ui.feed.FeedFragment.Companion.KEY_MOVIE_ID
 import ru.androidschool.intensiv.ui.feed.FeedFragment.Companion.KEY_SEARCH
+import ru.androidschool.intensiv.ui.feed.MovieItem
+import java.util.Locale
+import java.util.concurrent.TimeUnit
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
 
@@ -19,7 +36,18 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
     // onDestroyView.
     private val binding get() = _binding!!
     private val searchBinding get() = _searchBinding!!
-
+    private val adapter by lazy {
+        GroupAdapter<GroupieViewHolder>()
+    }
+    private val options = navOptions {
+        anim {
+            enter = R.anim.slide_in_right
+            exit = R.anim.slide_out_left
+            popEnter = R.anim.slide_in_left
+            popExit = R.anim.slide_out_right
+        }
+    }
+    private var disposables = CompositeDisposable()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,11 +62,69 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         super.onViewCreated(view, savedInstanceState)
         val searchTerm = requireArguments().getString(KEY_SEARCH)
         searchBinding.searchToolbar.setText(searchTerm)
+        search()
+    }
+
+    private fun search() = with(searchBinding) {
+        Observable.create(ObservableOnSubscribe<String> { subscriber ->
+            searchToolbar.binding.searchEditText.afterTextChanged {
+                subscriber.onNext(it.toString())
+            }
+        })
+            .debounce(500, TimeUnit.MILLISECONDS)
+            .filter { text -> text.isNotBlank() && text.length >= 3 }
+            .map { text -> text.lowercase(Locale.getDefault()).trim() }
+            .distinctUntilChanged()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { query ->
+                    val findMovies = MovieApiClient.apiClient.findMovies(query)
+                    disposables.add(
+                        findMovies
+                            .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(
+                                { result ->
+                                    val movieList =
+                                        result.results.map {
+                                            MovieItem(MovieSearchMapper().toViewObject(it)) { movie ->
+                                                openMovieDetails(
+                                                    movie
+                                                )
+                                            }
+                                        }.toList()
+                                    binding.moviesRecyclerView.adapter = adapter.apply {
+                                        if (movieList != null) {
+                                            addAll(movieList)
+                                        }
+                                    }
+                                }, {}
+                            )
+                    )
+
+                },
+                { error -> Log.d(TAG, "Error: ${error.message}") }
+            )
+    }
+
+    private fun openMovieDetails(movie: MovieLocal) {
+        val bundle = Bundle()
+        bundle.putInt(KEY_MOVIE_ID, movie.id)
+        findNavController().navigate(R.id.movie_details_fragment, bundle, options)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        disposables.clear()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
         _searchBinding = null
+    }
+
+    companion object {
+        const val TAG = "SearchFragment"
     }
 }
