69a2adf9af13b5bc93cd398820d3c3c9fa883e50
Artem Ternovenko <artem.inkaz@gmail.com>
2024-09-13 20:03:35 +0500

    [home_work_8] add MVI to Favourites Film

--- app/src/main/java/ru/androidschool/intensiv/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/presentation/profile/ProfileFragment.kt
@@ -6,18 +6,23 @@ import android.text.style.RelativeSizeSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.squareup.picasso.Picasso
 import jp.wasabeef.picasso.transformations.CropCircleTransformation
 import ru.androidschool.intensiv.R
 import ru.androidschool.intensiv.core.base.BaseFragment
-import ru.androidschool.intensiv.data.repositoryimpl.RepositoryHolder
+import ru.androidschool.intensiv.data.vo.MovieLocal
 import ru.androidschool.intensiv.databinding.FragmentProfileBinding
-import ru.androidschool.intensiv.extensions.applySchedulers
-import timber.log.Timber
+import ru.androidschool.intensiv.presentation.profile.mvi.UserIntention
+import ru.androidschool.intensiv.presentation.profile.mvi.ViewState
+import ru.androidschool.intensiv.presentation.profile.viewmodel.ProfileViewModel
+import ru.androidschool.intensiv.presentation.profile.viewmodel.ProfileViewModelFactory
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
+
+    private val viewModel: ProfileViewModel by viewModels { ProfileViewModelFactory() }
 
     private lateinit var profileTabLayoutTitles: Array<String>
 
@@ -32,7 +37,10 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
     private var profilePageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
         override fun onPageSelected(position: Int) {
             if (position == 0) {
-                getFavouritesFilm()
+                viewModel.observableState.observe(viewLifecycleOwner) { state ->
+                    renderState(state)
+                }
+                viewModel.dispatch(UserIntention.LoadMovies)
             } else {
                 countFavourite = 0
             }
@@ -62,38 +70,40 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
         )
     }
 
-    private fun getFavouritesFilm() = with(binding) {
-        val movieList = RepositoryHolder.repositoryMovieFromStorage().getFavouriteMovies()
-        disposables.add(
-            movieList
-                .applySchedulers()
-                .subscribe(
-                    {
-                        countFavourite = it.size
-                        TabLayoutMediator(
-                            tabLayout,
-                            profileViewPager
-                        ) { tab, position ->
-                            // Выделение первой части заголовка таба
-                            // Название таба
-                            var title = profileTabLayoutTitles[position]
-                            title =
-                                "${countFavourite} ${title.substring(title.lastIndexOf("\n") + 1)}"
-                            // Раздеряем название на части. Первый элемент будет кол-во
-                            val parts = title.split(" ")
-                            val number = parts[0]
-                            val spannableStringTitle = SpannableString(title)
-                            spannableStringTitle.setSpan(RelativeSizeSpan(2f), 0, number.count(), 0)
+    private fun renderState(state: ViewState) {
+        with(state) {
+            when {
+                isLoading -> renderLoadingState()
+                isError -> renderErrorState()
+                else -> renderMoviesState(items)
+            }
+        }
+    }
 
-                            tab.text = spannableStringTitle
-                        }.attach()
+    private fun renderLoadingState() {
+        // Show Progress Bar
+    }
 
-                    },
-                    {
-                        Timber.tag(TAG).d("Error doOnError: %s", it.message)
-                    }
-                )
-        )
+    private fun renderErrorState() {
+        viewModel.dispatch(UserIntention.ErrorShown)
+    }
+
+    private fun renderMoviesState(items: List<MovieLocal>) = with(binding) {
+        countFavourite = items.size
+        TabLayoutMediator(tabLayout, profileViewPager) { tab, position ->
+            // Выделение первой части заголовка таба
+            // Название таба
+            var title = profileTabLayoutTitles[position]
+            title =
+                "${countFavourite} ${title.substring(title.lastIndexOf("\n") + 1)}"
+            // Раздеряем название на части. Первый элемент будет кол-во
+            val parts = title.split(" ")
+            val number = parts[0]
+            val spannableStringTitle = SpannableString(title)
+            spannableStringTitle.setSpan(RelativeSizeSpan(2f), 0, number.count(), 0)
+
+            tab.text = spannableStringTitle
+        }.attach()
     }
 
     companion object {
